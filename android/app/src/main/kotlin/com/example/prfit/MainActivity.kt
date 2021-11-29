f0f22513b2cfbe201d1ccbe100f97c6f3b9dd039
Kamil <sikorakamil26@gmail.com>
2021-11-30 00:48:13 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/prfit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prfit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('PRFit'),
+        ),
+        body: const Center(
+          child: Text('Setup'),
+        ),
+      ),
+    );
+  }
+}
