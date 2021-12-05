0692f4119794323f83e1b92f7fe603dfdefa0b52
Kamil <sikorakamil26@gmail.com>
2021-12-05 14:51:23 +0100

    fix warnings

--- packages/pr_fit_ui/pr_fit_gallery/lib/main.dart
+++ packages/pr_fit_ui/pr_fit_gallery/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
+// ignore: public_member_api_docs
 class MyApp extends StatelessWidget {
+  // ignore: public_member_api_docs
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
