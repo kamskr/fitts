99fd4482e1a59488418f48ea599c3a16db8117bf
Kamil <sikorakamil26@gmail.com>
2021-12-05 14:47:19 +0100

    setup gallery

---
+++ packages/pr_fit_ui/pr_fit_gallery/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Gallery',
+          ),
+        ),
+        body: const Text('Gallery'),
+      ),
+    );
+  }
+}
