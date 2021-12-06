f78d801f8c0cd602d16d8798626632db7423221f
Kamil <sikorakamil26@gmail.com>
2021-12-07 00:02:23 +0100

    added typography

---
+++ packages/app_ui/app_gallery/lib/screens/typography_screen.dart
@@ -0,0 +1,63 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:flutter/material.dart';
+
+class TypographyScreen extends StatelessWidget {
+  const TypographyScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AppTypography'),
+      ),
+      body: ListView(
+        children: [
+          Text(
+            'title1',
+            style: AppTypography.title1,
+          ),
+          Text(
+            'title2',
+            style: AppTypography.title2,
+          ),
+          Text(
+            'title3',
+            style: AppTypography.title3,
+          ),
+          Text(
+            'title4',
+            style: AppTypography.title4,
+          ),
+          Text(
+            'title5',
+            style: AppTypography.title5,
+          ),
+          Text(
+            'headline',
+            style: AppTypography.headline,
+          ),
+          Text(
+            'body',
+            style: AppTypography.body,
+          ),
+          Text(
+            'subhead',
+            style: AppTypography.subhead,
+          ),
+          Text(
+            'footnote',
+            style: AppTypography.footnote,
+          ),
+          Text(
+            'caption1',
+            style: AppTypography.caption1,
+          ),
+          Text(
+            'caption2',
+            style: AppTypography.caption2,
+          ),
+        ],
+      ),
+    );
+  }
+}
