4153ee8455b21a5cae225d606cf2b3030270c013
Kamil <sikorakamil26@gmail.com>
2021-12-07 22:14:59 +0100

    added bottom navigation bar basic display

--- packages/app_ui/app_gallery/lib/screens/widgets_screen.dart
+++ packages/app_ui/app_gallery/lib/screens/widgets_screen.dart
@@ -1,15 +1,29 @@
 import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 
-class WidgetsScreen extends StatelessWidget {
+class WidgetsScreen extends StatefulWidget {
   const WidgetsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetsScreen> createState() => _WidgetsScreenState();
+}
+
+class _WidgetsScreenState extends State<WidgetsScreen> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Widgets')),
-      bottomNavigationBar: const AppBottomNavigationBar(
-          menuItems: ['Test', 'Test2'], onTap: print),
+      bottomNavigationBar: AppBottomNavigationBar(
+        menuItems: const ['Test', 'Test2'],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
       body: ListView(children: const [
         Text('Widgets'),
       ]),
--- packages/app_ui/lib/src/widgets/app_bottom_navigation_bar.dart
+++ packages/app_ui/lib/src/widgets/app_bottom_navigation_bar.dart
@@ -2,27 +2,31 @@ import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 
 class AppBottomNavigationBar extends StatelessWidget {
-  const AppBottomNavigationBar(
-      {required this._menuItems, required this._onTap, Key? key})
-      : super(key: key);
+  const AppBottomNavigationBar({
+    required this.menuItems,
+    required this.currentIndex,
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
 
-  final List<String> _menuItems;
-  final Function(int) _onTap;
+  final List<String> menuItems;
+  final int currentIndex;
+  final Function(int) onTap;
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       showUnselectedLabels: false,
-      items: _menuItems
+      items: menuItems
           .map((menuItem) => BottomNavigationBarItem(
                 icon: const Icon(Icons.ac_unit),
                 label: menuItem,
               ))
           .toList(),
       selectedItemColor: AppColors.black,
-      currentIndex: _selectedIndex,
-      onTap: _onTap,
+      currentIndex: currentIndex,
+      onTap: onTap,
     );
   }
 }
