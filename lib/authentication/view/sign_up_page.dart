b2445583cfe7f26a54b975b7805bddff1fe76b0a
Kamil <sikorakamil26@gmail.com>
2021-12-22 17:36:51 +0100

    feat: added sign up page

---
+++ lib/authentication/view/sign_up_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:prfit/l10n/l10n.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  static Route route() =>
+      MaterialPageRoute<void>(builder: (_) => const SignUpPage());
+
+  @override
+  Widget build(BuildContext context) {
+    return const SignUpView();
+  }
+}
+
+class SignUpView extends StatelessWidget {
+  const SignUpView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return Center(
+      child: Text(l10n.signUpPageTitle),
+    );
+  }
+}
