7e2d16e149a87ff4fe83d375cf4c9e9da0746951
Kamil <sikorakamil26@gmail.com>
2022-01-05 18:30:37 +0100

    refactor: move sign in and sign up to different folders

--- lib/welcome/view/view.dart
+++ lib/welcome/view/view.dart
@@ -1,3 +1,1 @@
-export 'sign_in_page.dart';
-export 'sign_up_page.dart';
 export 'welcome_page.dart';
--- lib/welcome/view/welcome_page.dart
+++ lib/welcome/view/welcome_page.dart
@@ -3,6 +3,8 @@ import 'package:authentication_client/authentication_client.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:prfit/l10n/l10n.dart';
+import 'package:prfit/sign_in/sign_in.dart';
+import 'package:prfit/sign_up/sign_up.dart';
 import 'package:prfit/welcome/welcome.dart';
 
 class WelcomePage extends StatelessWidget {
