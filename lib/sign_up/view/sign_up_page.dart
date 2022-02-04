279dd8df75054c827fbf2be582c46b9f8a1b617d
Kamil <sikorakamil26@gmail.com>
2022-02-04 19:56:25 +0100

    feat: added background color

--- lib/sign_up/view/sign_up_page.dart
+++ lib/sign_up/view/sign_up_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:prfit/l10n/l10n.dart';
 
@@ -20,10 +21,20 @@ class SignUpView extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = context.l10n;
 
-    return Scaffold(
-      appBar: AppBar(title: Text(l10n.signUpPageTitle)),
-      body: Center(
-        child: Text(l10n.signUpPageTitle),
+    return DecoratedBox(
+      decoration: const BoxDecoration(gradient: AppColors.primaryGradient3),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          iconTheme: const IconThemeData(
+            color: AppColors.white, //change your color here
+          ),
+        ),
+        body: Center(
+          child: Text(l10n.signUpPageTitle),
+        ),
       ),
     );
   }
