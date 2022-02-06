3e409dbefaf7e7a9fd9101dd28e563b9c69ed3ce
Kamil <sikorakamil26@gmail.com>
2022-02-06 18:00:40 +0100

    feat: prepare SignUpState

--- lib/sign_up/bloc/sign_up_bloc.dart
+++ lib/sign_up/bloc/sign_up_bloc.dart
@@ -1,11 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:form_validators/form_validators.dart';
+import 'package:formz/formz.dart';
 
 part 'sign_up_event.dart';
 part 'sign_up_state.dart';
 
 class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
-  SignUpBloc() : super(SignUpInitial()) {
+  SignUpBloc() : super(const SignUpState()) {
     on<SignUpEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/sign_up/bloc/sign_up_state.dart
+++ lib/sign_up/bloc/sign_up_state.dart
@@ -1,10 +1,32 @@
 part of 'sign_up_bloc.dart';
 
-abstract class SignUpState extends Equatable {
-  const SignUpState();
-  
-  @override
-  List<Object> get props => [];
-}
+class SignUpState extends Equatable {
+  const SignUpState({
+    this.username = const Username.pure(),
+    this.email = const Email.pure(),
+    this.password = const Password.pure(),
+    this.status = FormzStatus.pure,
+  });
 
-class SignUpInitial extends SignUpState {}
+  final Username username;
+  final Email email;
+  final Password password;
+  final FormzStatus status;
+
+  @override
+  List<Object> get props => [username, email, password, status];
+
+  SignUpState copyWith({
+    Username? username,
+    Email? email,
+    Password? password,
+    FormzStatus? status,
+  }) {
+    return SignUpState(
+      username: username ?? this.username,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      status: status ?? this.status,
+    );
+  }
+}
