a33a556c4704dfae6d112153d4b8a1b7575b1c12
Kamil <sikorakamil26@gmail.com>
2022-02-06 17:46:18 +0100

    feat: generated empty bloc

---
+++ lib/sign_up/bloc/sign_up_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'sign_up_event.dart';
+part 'sign_up_state.dart';
+
+class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
+  SignUpBloc() : super(SignUpInitial()) {
+    on<SignUpEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/sign_up/bloc/sign_up_state.dart
@@ -0,0 +1,10 @@
+part of 'sign_up_bloc.dart';
+
+abstract class SignUpState extends Equatable {
+  const SignUpState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class SignUpInitial extends SignUpState {}
