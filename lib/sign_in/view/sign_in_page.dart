ea70d198fd9174edb2d9112ba68de4d0df186d4e
Kamil <sikorakamil26@gmail.com>
2022-02-08 23:06:41 +0100

    feat: added SignIn page logic

--- lib/sign_in/view/sign_in_page.dart
+++ lib/sign_in/view/sign_in_page.dart
@@ -1,6 +1,10 @@
 import 'package:app_ui/app_ui.dart';
+import 'package:authentication_client/authentication_client.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
 import 'package:prfit/l10n/l10n.dart';
+import 'package:prfit/sign_in/sign_in.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -10,7 +14,10 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SignInView();
+    return BlocProvider(
+      create: (context) => SignInBloc(context.read<AuthenticationClient>()),
+      child: const SignInView(),
+    );
   }
 }
 
@@ -19,31 +26,54 @@ class SignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: const BoxDecoration(
-        gradient: AppColors.primaryGradient3,
-      ),
-      child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
+    return _SignInBlocListener(
+      child: DecoratedBox(
+        decoration: const BoxDecoration(
+          gradient: AppColors.primaryGradient3,
+        ),
+        child: Scaffold(
             backgroundColor: Colors.transparent,
-            iconTheme: const IconThemeData(
-              color: AppColors.white, //change your color here
-            ),
-          ),
-          body: SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  _SignInTitle(),
-                  _EmailInput(),
-                  _PasswordInput(),
-                  _SignUpButton(),
-                ],
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              iconTheme: const IconThemeData(
+                color: AppColors.white, //change your color here
               ),
             ),
-          )),
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    _SignInTitle(),
+                    _EmailInput(),
+                    _PasswordInput(),
+                    _SignInButton(),
+                  ],
+                ),
+              ),
+            )),
+      ),
+    );
+  }
+}
+
+class _SignInBlocListener extends StatelessWidget {
+  const _SignInBlocListener({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<SignInBloc, SignInState>(
+      listener: (context, state) {
+        if (state.status.isSubmissionSuccess) {
+          Navigator.pop(context);
+        }
+      },
+      child: child,
     );
   }
 }
@@ -75,6 +105,7 @@ class _EmailInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
+    final email = context.select((SignInBloc bloc) => bloc.state.email);
 
     return Padding(
       padding: const EdgeInsets.only(
@@ -83,6 +114,10 @@ class _EmailInput extends StatelessWidget {
       child: AppTextField(
         labelText: l10n.signInPageEmailLabel,
         inputType: AppTextFieldType.email,
+        errorText: email.invalid ? l10n.signUpPageEmailErrorMessage : null,
+        onChanged: (newValue) {
+          context.read<SignInBloc>().add(SignInEmailChanged(newValue));
+        },
       ),
     );
   }
@@ -94,6 +129,7 @@ class _PasswordInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
+    final password = context.select((SignInBloc bloc) => bloc.state.password);
 
     return Padding(
       padding: const EdgeInsets.only(
@@ -102,13 +138,18 @@ class _PasswordInput extends StatelessWidget {
       child: AppTextField(
         labelText: l10n.signInPagePasswordLabel,
         inputType: AppTextFieldType.password,
+        errorText:
+            password.invalid ? l10n.signUpPagePasswordErrorMessage : null,
+        onChanged: (newValue) {
+          context.read<SignInBloc>().add(SignInPasswordChanged(newValue));
+        },
       ),
     );
   }
 }
 
-class _SignUpButton extends StatelessWidget {
-  const _SignUpButton({Key? key}) : super(key: key);
+class _SignInButton extends StatelessWidget {
+  const _SignInButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -116,9 +157,16 @@ class _SignUpButton extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.all(AppSpacing.xxlg),
-      child: AppButton.gradient(
-        onPressed: () {},
-        child: Text(l10n.signInPageSignUpButton),
+      child: BlocBuilder<SignInBloc, SignInState>(
+        builder: (context, state) {
+          return AppButton.gradient(
+            onPressed: () {
+              context.read<SignInBloc>().add(SignInCredentialsSubmitted());
+            },
+            isLoading: state.status == FormzStatus.submissionInProgress,
+            child: Text(l10n.signInPageSignInButton),
+          );
+        },
       ),
     );
   }
