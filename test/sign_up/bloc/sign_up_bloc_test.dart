58dae18430043b49307886a517d2bdc837d49bcd
Kamil <sikorakamil26@gmail.com>
2022-02-09 21:51:58 +0100

    test: added tests for sign_up bloc

---
+++ test/sign_up/bloc/sign_up_bloc_test.dart
@@ -0,0 +1,138 @@
+import 'package:authentication_client/authentication_client.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:form_validators/form_validators.dart';
+import 'package:formz/formz.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:prfit/sign_up/sign_up.dart';
+
+class MockAuthenticationClient extends Mock implements AuthenticationClient {}
+
+void main() {
+  const invalidUsernameString = 't';
+  const invalidUsername = Username.dirty(invalidUsernameString);
+
+  const validUsernameString = 'testTest';
+  const validUsername = Username.dirty(validUsernameString);
+
+  const invalidEmailString = 'test';
+  const invalidEmail = Email.dirty(invalidEmailString);
+
+  const validEmailString = 'test@gmail.com';
+  const validEmail = Email.dirty(validEmailString);
+
+  const invalidPasswordString = 'invalid';
+  const invalidPassword = Password.dirty(invalidPasswordString);
+
+  const validPasswordString = 't0pS3cret1234!!';
+  const validPassword = Password.dirty(validPasswordString);
+
+  group('SignUpBloc', () {
+    late AuthenticationClient authenticationRepository;
+
+    setUp(() {
+      authenticationRepository = MockAuthenticationClient();
+      when(
+        () => authenticationRepository.signUp(
+          email: any(named: 'email'),
+          password: any(named: 'password'),
+        ),
+      ).thenAnswer((_) async {});
+    });
+
+    group('username changed', () {
+      blocTest<SignUpBloc, SignUpState>(
+        'emits [invalid] when username/email/password are invalid',
+        build: () => SignUpBloc(authenticationRepository),
+        act: (bloc) => bloc.add(
+          const SignUpUsernameChanged(invalidUsernameString),
+        ),
+        expect: () => const <SignUpState>[
+          SignUpState(username: invalidUsername, status: FormzStatus.invalid),
+        ],
+      );
+
+      blocTest<SignUpBloc, SignUpState>(
+        'emits [valid] when email/password/confirmedPassword are valid',
+        build: () => SignUpBloc(authenticationRepository),
+        seed: () => const SignUpState(
+          email: validEmail,
+          password: validPassword,
+        ),
+        act: (bloc) =>
+            bloc.add(const SignUpUsernameChanged(validUsernameString)),
+        expect: () => const <SignUpState>[
+          SignUpState(
+            username: validUsername,
+            email: validEmail,
+            password: validPassword,
+            status: FormzStatus.valid,
+          ),
+        ],
+      );
+    });
+
+    group('email changed', () {
+      blocTest<SignUpBloc, SignUpState>(
+        'emits [invalid] when username/email/password are invalid',
+        build: () => SignUpBloc(authenticationRepository),
+        act: (bloc) => bloc.add(
+          const SignUpEmailChanged(invalidEmailString),
+        ),
+        expect: () => const <SignUpState>[
+          SignUpState(email: invalidEmail, status: FormzStatus.invalid),
+        ],
+      );
+
+      blocTest<SignUpBloc, SignUpState>(
+        'emits [valid] when email/password/confirmedPassword are valid',
+        build: () => SignUpBloc(authenticationRepository),
+        seed: () => const SignUpState(
+          username: validUsername,
+          password: validPassword,
+        ),
+        act: (bloc) => bloc.add(const SignUpEmailChanged(validEmailString)),
+        expect: () => const <SignUpState>[
+          SignUpState(
+            username: validUsername,
+            email: validEmail,
+            password: validPassword,
+            status: FormzStatus.valid,
+          ),
+        ],
+      );
+    });
+
+    group('password changed', () {
+      blocTest<SignUpBloc, SignUpState>(
+        'emits [invalid] when username/email/password are invalid',
+        build: () => SignUpBloc(authenticationRepository),
+        act: (bloc) => bloc.add(
+          const SignUpPasswordChanged(invalidPasswordString),
+        ),
+        expect: () => const <SignUpState>[
+          SignUpState(password: invalidPassword, status: FormzStatus.invalid),
+        ],
+      );
+
+      blocTest<SignUpBloc, SignUpState>(
+        'emits [valid] when email/password/confirmedPassword are valid',
+        build: () => SignUpBloc(authenticationRepository),
+        seed: () => const SignUpState(
+          email: validEmail,
+          username: validUsername,
+        ),
+        act: (bloc) =>
+            bloc.add(const SignUpPasswordChanged(validPasswordString)),
+        expect: () => const <SignUpState>[
+          SignUpState(
+            username: validUsername,
+            email: validEmail,
+            password: validPassword,
+            status: FormzStatus.valid,
+          ),
+        ],
+      );
+    });
+  });
+}
---
+++ test/sign_up/bloc/sign_up_event_test.dart
@@ -0,0 +1,72 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:prfit/sign_up/sign_up.dart';
+
+void main() {
+  group('SignUpEvent', () {
+    group('SignUpUsernameChanged', () {
+      const username = 'testUsername';
+      test('supports value comparisons', () {
+        expect(
+          const SignUpUsernameChanged(username),
+          equals(const SignUpUsernameChanged(username)),
+        );
+      });
+
+      test('props are correct', () {
+        expect(
+          const SignUpUsernameChanged('username').props,
+          equals(<Object?>[
+            'username', // title
+          ]),
+        );
+      });
+    });
+
+    group('SignUpEmailChanged', () {
+      const email = 'test@test.pl';
+      test('supports value comparisons', () {
+        expect(
+          const SignUpEmailChanged(email),
+          equals(const SignUpEmailChanged(email)),
+        );
+      });
+
+      test('props are correct', () {
+        expect(
+          const SignUpEmailChanged('email').props,
+          equals(<Object?>[
+            'email', // title
+          ]),
+        );
+      });
+    });
+
+    group('SignUpPasswordChanged', () {
+      const password = 'testPassword';
+      test('supports value comparisons', () {
+        expect(
+          const SignUpPasswordChanged(password),
+          equals(const SignUpPasswordChanged(password)),
+        );
+      });
+
+      test('props are correct', () {
+        expect(
+          const SignUpPasswordChanged('password').props,
+          equals(<Object?>[
+            'password', // title
+          ]),
+        );
+      });
+    });
+
+    group('SignUpCredentialsSubmitted', () {
+      test('supports value comparisons', () {
+        expect(
+          SignUpCredentialsSubmitted(),
+          equals(SignUpCredentialsSubmitted()),
+        );
+      });
+    });
+  });
+}
