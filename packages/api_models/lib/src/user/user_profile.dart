040733fb5ef4f16f6d5fec89c9eae13a34d93526
Kamil <sikorakamil26@gmail.com>
2022-04-09 20:19:25 +0200

    finish user model and add melos

--- packages/api_models/lib/src/user/user_profile.dart
+++ packages/api_models/lib/src/user/user_profile.dart
@@ -3,6 +3,17 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'user_profile.g.dart';
 
+/// {@template gender}
+/// Represents gender of the user
+/// {@endtemplate}
+enum Gender {
+  /// Male
+  male,
+
+  /// Female
+  female,
+}
+
 /// {@template user_profile}
 /// A model representing user profile stored in the firestore realtime database.
 ///
@@ -16,6 +27,11 @@ class UserProfile extends Equatable {
     required this.email,
     required this.photoUrl,
     required this.displayName,
+    required this.goal,
+    required this.gender,
+    required this.dateOfBirth,
+    required this.height,
+    required this.weight,
   });
 
   /// Factory which converts a [Map] into a [UserProfile].
@@ -34,11 +50,31 @@ class UserProfile extends Equatable {
   /// The display name of the user.
   final String displayName;
 
+  /// Current goal of this user (loose weight, gain weight, etc).
+  final String goal;
+
+  /// Gender of the user.
+  final Gender gender;
+
+  /// Birth date of the user.
+  final DateTime dateOfBirth;
+
+  /// Height of the user.
+  final int height;
+
+  /// Weight if the user.
+  final double weight;
+
   /// An empty [UserProfile] object.
-  static const empty = UserProfile(
+  static UserProfile empty = UserProfile(
     email: '',
     photoUrl: '',
     displayName: '',
+    goal: '',
+    gender: Gender.male,
+    dateOfBirth: DateTime.now(),
+    height: 0,
+    weight: 0,
   );
 
   /// Creates a copy of [UserProfile].
@@ -46,19 +82,49 @@ class UserProfile extends Equatable {
     String? email,
     String? photoUrl,
     String? displayName,
+    String? goal,
+    Gender? gender,
+    DateTime? dateOfBirth,
+    int? height,
+    double? weight,
   }) {
     return UserProfile(
       email: email ?? this.email,
       photoUrl: photoUrl ?? this.photoUrl,
       displayName: displayName ?? this.displayName,
+      goal: goal ?? this.goal,
+      gender: gender ?? this.gender,
+      dateOfBirth: dateOfBirth ?? this.dateOfBirth,
+      height: height ?? this.height,
+      weight: weight ?? this.weight,
     );
   }
 
   @override
-  List<Object> get props => [email, photoUrl, displayName];
+  List<Object> get props {
+    return [
+      email,
+      photoUrl,
+      displayName,
+      goal,
+      gender,
+      dateOfBirth,
+      height,
+      weight,
+    ];
+  }
 
   @override
-  String toString() => 'UserProfile(email: $email, '
-      'photoUrl: $photoUrl, '
-      'displayName: $displayName)';
+  String toString() {
+    return 'UserProfile('
+        'email: $email, '
+        'photoUrl: $photoUrl, '
+        'displayName: $displayName, '
+        'goal: $goal, '
+        'gender: $gender, '
+        'dateOfBirth: $dateOfBirth, '
+        'height: $height, '
+        'weight: $weight'
+        ')';
+  }
 }
--- packages/api_models/lib/src/user/user_profile.g.dart
+++ packages/api_models/lib/src/user/user_profile.g.dart
@@ -10,6 +10,11 @@ UserProfile _$UserProfileFromJson(Map<String, dynamic> json) => UserProfile(
       email: json['email'] as String,
       photoUrl: json['photoUrl'] as String,
       displayName: json['displayName'] as String,
+      goal: json['goal'] as String,
+      gender: $enumDecode(_$GenderEnumMap, json['gender']),
+      dateOfBirth: DateTime.parse(json['dateOfBirth'] as String),
+      height: json['height'] as int,
+      weight: (json['weight'] as num).toDouble(),
     );
 
 Map<String, dynamic> _$UserProfileToJson(UserProfile instance) =>
@@ -17,4 +22,14 @@ Map<String, dynamic> _$UserProfileToJson(UserProfile instance) =>
       'email': instance.email,
       'photoUrl': instance.photoUrl,
       'displayName': instance.displayName,
+      'goal': instance.goal,
+      'gender': _$GenderEnumMap[instance.gender],
+      'dateOfBirth': instance.dateOfBirth.toIso8601String(),
+      'height': instance.height,
+      'weight': instance.weight,
     };
+
+const _$GenderEnumMap = {
+  Gender.male: 'male',
+  Gender.female: 'female',
+};
