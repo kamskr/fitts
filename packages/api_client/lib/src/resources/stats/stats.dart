bfd2b6863ebc13dd21f442ffa3df15d42fcbef13
Kamil Sikora <sikorakamil26@gmail.com>
2022-07-31 21:31:55 +0200

    prepared innitial stats resource

---
+++ packages/api_client/lib/src/resources/stats/stats.dart
@@ -0,0 +1,1 @@
+export 'user_stats_resource.dart';
---
+++ packages/api_client/lib/src/resources/stats/user_stats_resource.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:api_client/api_client.dart';
+import 'package:api_models/api_models.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+/// {@template user_stats_resource}
+/// Resource which exposes APIs related to user stats.
+/// {@endtemplate}
+class UserStatsResource {
+  /// {@macro user_stats_resource}
+  UserStatsResource(FirebaseFirestore firebaseFirestore)
+      : _firebaseFirestore = firebaseFirestore;
+
+  final FirebaseFirestore _firebaseFirestore;
+
+  static const _collectionName = 'UserStats';
+
+  /// Stream of [UserStats] data for user with id [userId].
+  Stream<UserStats?> userStars(String userId) {
+    try {
+      return _firebaseFirestore
+          .collection(_collectionName)
+          .doc(userId)
+          .snapshots()
+          .map<UserStats?>((documentSnapshot) {
+        final data = documentSnapshot.data();
+        if (documentSnapshot.exists && data != null) {
+          try {
+            return UserStats.fromJson(data);
+          } catch (exception, stackTrace) {
+            throw DeserializationException(exception, stackTrace);
+          }
+        } else {
+          return null;
+        }
+      }).transform(StreamTransformer.fromHandlers(
+        handleError: (e, st, sink) {
+          sink.addError(e);
+        },
+      ));
+    } on Exception catch (e, st) {
+      throw ApiException(e, st);
+    }
+  }
+}
