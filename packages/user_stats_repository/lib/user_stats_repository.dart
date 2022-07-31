a8cc55fbb5e246537e948cf51a1461d05ff2ffc8
Kamil Sikora <sikorakamil26@gmail.com>
2022-07-31 21:13:41 +0200

    add user stats exceptions

--- packages/user_stats_repository/lib/user_stats_repository.dart
+++ packages/user_stats_repository/lib/user_stats_repository.dart
@@ -1,7 +1,3 @@
 library user_stats_repository;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/user_stats_repository.dart';
