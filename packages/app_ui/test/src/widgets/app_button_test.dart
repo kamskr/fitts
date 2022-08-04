197534ff415765dd7f209aac2c04c0bb5e07dc7e
Kamil Sikora <sikorakamil26@gmail.com>
2022-08-04 22:36:59 +0200

    fix existsing tests

--- packages/app_ui/test/src/widgets/app_button_test.dart
+++ packages/app_ui/test/src/widgets/app_button_test.dart
@@ -1,7 +1,7 @@
 import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../helpers/helpers.dart';
+import '../../helpers/helpers.dart';
 
 void main() {
   group('AppButton.primary()', () {
