316710a928c3f6cb030f5c11ccd9e6eb6ffd36ef
Kamil Sikora <sikorakamil26@gmail.com>
2022-10-19 00:20:44 +0200

    fix tests

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -3,6 +3,7 @@ import 'package:app_ui/app_ui.dart';
 import 'package:authentication_client/authentication_client.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:fitts/app/app.dart';
+import 'package:fitts/home/home.dart';
 import 'package:fitts/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,6 +12,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:user_profile_repository/user_profile_repository.dart';
 import 'package:user_stats_repository/user_stats_repository.dart';
+import 'package:workouts_repository/workouts_repository.dart';
 
 class FakeAppState extends Fake implements AppState {}
 
@@ -28,6 +30,8 @@ class MockAppBloc extends MockBloc<AppEvent, AppState> implements AppBloc {
       );
 }
 
+class MockHomeBloc extends Mock implements HomeBloc {}
+
 class MockAuthenticationClient extends Mock implements AuthenticationClient {
   @override
   Stream<User> get user => Stream.value(User.empty);
@@ -40,13 +44,17 @@ class MockUserStatsRepository extends Mock implements UserStatsRepository {
   Stream<UserStats> get userStats => Stream.value(UserStats.empty);
 }
 
+class MockWorkoutsRepository extends Mock implements WorkoutsRepository {}
+
 extension AppTester on WidgetTester {
   Future<void> pumpApp(
     Widget widgetUnderTest, {
     AppBloc? appBloc,
+    HomeBloc? homeBloc,
     AuthenticationClient? authenticationClient,
     UserProfileRepository? userProfileRepository,
     UserStatsRepository? userStatsRepository,
+    MockWorkoutsRepository? workoutsRepository,
   }) async {
     await pumpWidget(
       MultiRepositoryProvider(
@@ -60,11 +68,17 @@ extension AppTester on WidgetTester {
           RepositoryProvider.value(
             value: userStatsRepository ?? MockUserStatsRepository(),
           ),
+          RepositoryProvider.value(
+            value: workoutsRepository ?? MockWorkoutsRepository(),
+          ),
         ],
         child: MultiBlocProvider(
           providers: [
             BlocProvider.value(
               value: appBloc ?? MockAppBloc(),
+            ),
+            BlocProvider.value(
+              value: homeBloc ?? MockHomeBloc(),
             ),
           ],
           child: MaterialApp(
